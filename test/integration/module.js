950a3523e909ff08d83e7bfb63d6d62173565340
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-03-06 12:54:04 +0100

    test: make test run with no-empty-source rule

--- test/integration/module.js
+++ test/integration/module.js
@@ -3,7 +3,7 @@ const stylelint = require('stylelint');
 
 describe('stylelint-config-holy-grail', () => {
     it('should lint a dummy text', () => {
-        return stylelint.lint({ code: '', config }).then((data) => {
+        return stylelint.lint({ code: '.class { color: #fff; }', config }).then((data) => {
             expect(data.errored).to.be.false;
 
             expect(data.results[0].warnings.length).to.equal(0);
